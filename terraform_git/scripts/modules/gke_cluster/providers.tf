db9fd58b75be1d72fb9e6b928006bc7a7678a7f6
Sushanta-lab <63727277+Sushanta-lab@users.noreply.github.com>
2022-06-29 22:24:54 +0530

    Update providers.tf

--- terraform_git/scripts/modules/gke_cluster/providers.tf
+++ terraform_git/scripts/modules/gke_cluster/providers.tf
@@ -1,7 +1,7 @@
 provider "google" {
   project = var.project_id
   region  = var.region
-  credentials = "../../serviceaccount.json"
+  credentials = "../serviceaccount.json"
   #access_token = var.access_token
   #zone    = var.zone
 }
