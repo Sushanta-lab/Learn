b5125c70b5d8b56ea01cf36e5f0204ce0792cd42
Sushanta-lab <63727277+Sushanta-lab@users.noreply.github.com>
2022-06-30 11:34:31 +0530

    Delete terraform_git/scripts/modules/docker-image directory

--- terraform_git/scripts/modules/docker-image/env.sh
+++
@@ -1,8 +0,0 @@
-export DOCKER_REGISTRY="gcr.io/creating-and-61-efdbbd57"
-export VERSION="v2"
-
-set -u # or set -o nounset
-: "$VERSION"
-
-#!/bin/sh
-sh -c "sed -i 's/tag: VERSION/tag: $VERSION/g' ../../../charts/spring/values.yaml"
